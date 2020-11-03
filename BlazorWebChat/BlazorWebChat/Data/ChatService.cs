95cf966ef4df7ac881cbb7bedee6a3a643089682
yunwei.xiao <yunwei.xiao@capgemini.com>
2020-11-03 14:40:41 +0800

    add BlazorWebChat

---
+++ BlazorWebChat/BlazorWebChat/Data/ChatService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebChat.Data
+{
+    public interface IChatService
+    {
+        string GetReplyText(string message);
+    }
+
+    public class ChatService : IChatService
+    {
+        public string GetReplyText(string message)
+        {
+            string resp = "";
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                resp = message.Trim().Replace("吗", "")
+                  .Replace("？", "！")
+                  .Replace("?", "!");
+            }
+
+            return resp;
+        }        
+    }
+}
---
+++ BlazorWebChat/BlazorWebChat/Data/Conversation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebChat.Data
+{
+    public class Conversation
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+    }
+}
